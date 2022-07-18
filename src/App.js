92f8b6ac10f11679a8e6234440fdede8b1d2d1fb
Alfred Boateng <programmingwithalfred@gmail.com>
2022-07-18 15:06:44 +0100

    Import app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Counter from './features/counter/counter';
 import Missions from './features/missions/missions';
+import RocketsIndex from './features/rockets/rockets';
 
 function App() {
   return (
     <Routes>
-      <Route exact path="/" element={<Missions />} />
+      <Route exact path="/" element={<RocketsIndex />} />
       <Route exact path="counter" element={<Counter />} />
+      <Route exact path="missions" element={<Missions />} />
     </Routes>
   );
 }
