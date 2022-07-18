9efc0fde1e1909370da334055d0c13b5b65a6ce9
Alfred Boateng <programmingwithalfred@gmail.com>
2022-07-18 09:59:57 +0100

    Setup linters and readme

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { store } from './app/store';
 import { Provider } from 'react-redux';
+import store from './app/store';
 import App from './App';
 
 const container = document.getElementById('root');
@@ -10,5 +10,5 @@ const root = createRoot(container);
 root.render(
   <Provider store={store}>
     <App />
-  </Provider>
+  </Provider>,
 );
