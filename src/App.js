ebb7c7f067e2255116f0e9dbc9aecfbe217d5039
Alfred Boateng <programmingwithalfred@gmail.com>
2022-07-18 09:48:42 +0100

    Setup redux and redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,2 +1,13 @@
-const App = () => <div>hello </div>;
+import Counter from './features/counter/counter';
+
+function App() {
+  return (
+    <div>
+      <h1>hello</h1>
+      <p>world</p>
+      <Counter />
+    </div>
+  );
+}
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
