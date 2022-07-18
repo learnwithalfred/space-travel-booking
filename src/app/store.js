2a099b6acb6c8159eefee79c680d2e65c4b90b48
Alfred Boateng <programmingwithalfred@gmail.com>
2022-07-18 15:08:42 +0100

    Add rockets to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import rocketReducer from '../features/rockets/rocketSlice';
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
+    rockets: rocketReducer,
   },
 });
 
