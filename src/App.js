d5bf4e040a168ede46f11becc6b5743aa40dcc54
Alfred Boateng <programmingwithalfred@gmail.com>
2022-07-18 16:40:12 +0100

    Nav link should be referenced in the App file
    This will allow keep the code DRY

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
 import Counter from './features/counter/counter';
+import NavHeader from './components/header/header';
 import Missions from './features/missions/missions';
 import RocketsIndex from './features/rockets/rockets';
 
 function App() {
   return (
-    <Routes>
-      <Route exact path="/" element={<RocketsIndex />} />
-      <Route exact path="counter" element={<Counter />} />
-      <Route exact path="missions" element={<Missions />} />
-    </Routes>
+    <>
+      <NavHeader />
+      <Routes>
+        <Route exact path="/" element={<RocketsIndex />} />
+        <Route exact path="counter" element={<Counter />} />
+        <Route exact path="missions" element={<Missions />} />
+      </Routes>
+    </>
   );
 }
 export default App;
