69947c79a8a9b0b2969ed0cd4eb2f4d453a55eb9
Alfred Boateng <programmingwithalfred@gmail.com>
2022-07-19 14:43:36 +0100

    Render reserved Rockets on screen

--- src/features/profile/profile.js
+++ src/features/profile/profile.js
@@ -1,32 +1,62 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  myReservedRockets,
+  getReservedRockets,
+  getRocketsStatus,
+} from '../rockets/rocketSlice';
 
-const ProfilePage = () => (
-  <div className="container my-4">
-    <div className="row align-items-md-stretch">
-      <div className="col-md-6">
-        <div className="h-100 p-2 text-dark bg-light rounded-3">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
-            tempore nesciunt quos illo asperiores aspernatur fugit maxime saepe
-            nostrum natus. Eligendi, omnis libero!
-          </p>
+const ProfilePage = () => {
+  const dispatch = useDispatch();
+  const rockets = useSelector(getReservedRockets);
+  const rocketStatus = useSelector(getRocketsStatus);
+
+  useEffect(() => {
+    dispatch(myReservedRockets());
+  }, [rocketStatus, dispatch]);
+
+  let renderReserved = '';
+  if (rockets.length) {
+    renderReserved = rockets.map((data) => (
+      <li
+        key={data.id}
+        className="list-group-item d-flex justify-content-between align-items-start"
+      >
+        <span>{data.rocket_name}</span>
+
+        <a
+          className="btn btn-link"
+          href={data.wikipedia}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Read more
+        </a>
+      </li>
+    ));
+  }
+
+  return (
+    <div className="container my-4">
+      <div className="row align-items-md-stretch">
+        <div className="col-md-6">
+          <div className="h-100 p-2 text-dark bg-light rounded-3">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
+              tempore nesciunt quos illo asperiores aspernatur fugit maxime
+              saepe nostrum natus. Eligendi, omnis libero!
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="col-md-6">
-        <div className="h-100 p-2 bg-light rounded-3">
-          <h2>My Rockets</h2>
-          <table className="table table-hover">
-            <tr>Mark</tr>
-            <tr>Mark</tr>
-            <tr>
-              <td>Mark</td>
-            </tr>
-            <tr>Mark</tr>
-          </table>
+        <div className="col-md-6">
+          <div className="h-100 p-2 rounded-3">
+            <h2>My Rockets</h2>
+            <ul className="list-group">{renderReserved}</ul>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ProfilePage;
